0cac20be03cebbc5d4369f5df6c1b8b6ab339273
Tilson Mateus <tilsonmat@gmail.com>
2024-03-17 16:00:46 +0100

    wip

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+
+      <h1 className="text-3xl text-red-900 font-bold underline">
+        Hello world!
+      </h1>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('daisyui')]
+}
