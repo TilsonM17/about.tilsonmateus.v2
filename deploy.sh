ec4ebfe82d90bbcb1b50eae97d3e82f4eba19cd8
Tilson Mateus <63884331+TilsonM17@users.noreply.github.com>
2024-03-19 18:44:46 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,4 @@ cd dist
 # if you are deploying to a custom domain
 echo 'about.tilsonmateus.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+cd ..
